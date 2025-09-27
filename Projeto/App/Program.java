a93446b7b9e448c2803eedb58bf0f09e834ca650
MatheusDS99 <dantas.sdm@gmail.com>
2025-09-26 22:11:57 -0300

    Inicio

---
+++ Projeto/App/Program.java
@@ -0,0 +1,16 @@
+package App;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import Entities.*;
+
+public class Program {
+    public static void main (String [] args){
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
